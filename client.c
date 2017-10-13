ed58f3abd87701ea87906d2a7d82c0ee2f3643d1
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-10-13 14:03:25 -0300

    mas comentarios

--- client.c
+++ client.c
@@ -50,8 +50,11 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
 
 
+		/* Conexion */
     if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
+
+
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
