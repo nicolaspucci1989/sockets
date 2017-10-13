0007bb5a751c305468988dae89c0f270e44661d3
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-10-13 13:55:31 -0300

    algunos comentarios

--- client.c
+++ client.c
@@ -17,13 +17,16 @@ int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
+		/* La variable server es un puntero a una estructura tipo
+			 hostent. Define a un host en internet */
     struct hostent *server;
-
     char buffer[256];
-    if (argc < 3) {
+
+    if (argc < 3) { // Chequeo de argumentos
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
