1902f09bfaef282080d085d1446f72d25354f35c
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-10-13 14:00:26 -0300

    mas comentarios

--- client.c
+++ client.c
@@ -27,21 +27,29 @@ int main(int argc, char *argv[])
        exit(0);
     }
 
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
+
+
     server = gethostbyname(argv[1]);
+		// gethostbyname devuelve un puntero a un hostent
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
+
+
     if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     printf("Please enter the message: ");
